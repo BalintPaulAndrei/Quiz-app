914414ea4db3de704d652030e5a926d4fc999feb
Paul <paulbalint2000@yahoo.com>
2021-05-19 15:42:29 +0300

    UPDATE:Created Login window

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText loginText, passwordText;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginText = (EditText) findViewById(R.id.login);
+        passwordText = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String  username = loginText.getText().toString();
+                String  password = passwordText.getText().toString();
+
+                if(username.equals("user") && (password.equals("1234")))
+                {
+                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), NewActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
