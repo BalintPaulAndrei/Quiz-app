bb7dd4fa7655be3610b317ddcf64870c3874000e
alinblendea <alinblendea1708@gmail.com>
2021-06-28 04:46:42 +0300

    A new GUI design for the question window
    Added the gui for the question window, with a progress bar and textviews as buttons for the answers

--- Quizz App V2.0/app/src/main/java/com/example/quizzappv20/QuestionsActivity.kt
+++ Quizz App V2.0/app/src/main/java/com/example/quizzappv20/QuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizzappv20
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_questions.*
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,18 @@ class QuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_questions)
 
         val questionsList = Constants.getQuestions()
-        Log.i("Questions size: ", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        progressBar3.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar3.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
     }
 }
